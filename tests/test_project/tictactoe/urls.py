b05ef21fcb89dbcb3d8ee298ae1e48747455c576
cookster9 <acook999@gmail.com>
2024-12-12 23:04:28 -0600

    fixed url, fixed test project to pull relative url, updated version

--- tests/test_project/tictactoe/urls.py
+++ tests/test_project/tictactoe/urls.py
@@ -19,6 +19,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("tictactoe/", include("tictactoe_app.urls")),
+    path("", include("tictactoe_app.urls")),
     path("admin/", admin.site.urls),
 ]
